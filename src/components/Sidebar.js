a98145ad7402fd89c11ad959807b9e3606daf037
Satyansh <satyansh.g16@gmail.com>
2025-05-16 14:31:09 +0530

    git commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { FaHome, FaFileArchive, FaUser, FaProjectDiagram, FaFileAlt, FaEnvelope } from 'react-icons/fa';
-import profile from '../assets/profile.jpg';
+import profile from '../assets/Satyansh_Image.jpg';
 
 const Sidebar = () => (
   <aside className="sidebar">
