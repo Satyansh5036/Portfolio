fa9cd8148bd5613067ed8cd5ed8986e46f741b75
Satyansh5036 <satyansh.g16@gmail.com>
2025-06-24 11:32:49 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -52,7 +52,7 @@ const Sidebar = ({ isOpen, closeSidebar, theme, toggleTheme }) => {
             />
             Projects
           </a>
-          <a href="https://drive.google.com/file/d/1P2HrNvtZPdduL07yR34F58Xe56_wKqEI/view?usp=sharing"  target="_blank" onClick={closeSidebar}>
+          <a href="https://drive.google.com/file/d/1b-FLy9_icJrh0mICZ2nvL9mz9Cfoh9U8/view?usp=sharing"  target="_blank" onClick={closeSidebar}>
             <TiDocumentText
               style={{ color: "#bf00ff", width: 24, height: 24, marginRight: 14 }}
             />
